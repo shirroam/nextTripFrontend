87dd1e00ab719353c6a8b0c8927d6c9f8b5be638
Shirley <smbiayuk@gmail.com>
2024-10-16 19:41:55 +0200

    Trips page created, no content yet

--- src/Router.js
+++ src/Router.js
@@ -2,6 +2,7 @@ import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Homepage from "./webPages/Homepage/Homepage";
 import Header from "./webComponents/Header/Header";
+import Trips from "./webPages/Trips/Trips";
 
 //This Router file will contain references to all webpages and accompanying routes within the project
 export default function Router() {
@@ -11,6 +12,7 @@ export default function Router() {
         <Routes>
           {/* <Route path="" element={<Login />} /> */}
           <Route path="/homePage" element={<Homepage />} />
+          <Route path="/trips" element={<Trips />} />
           {/* <Route path="/Trainer" element={<HomepageTrainer />} /> */}
         </Routes>
       </BrowserRouter>
---
+++ src/webPages/Trips/Trips.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import './Trips.css';
+
+function Trips() {
+
+}
+
+export default Trips;
