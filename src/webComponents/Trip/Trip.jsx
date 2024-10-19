dd3c7312f270648215c8b8eb00f2667aa7b20468
Shirley <smbiayuk@gmail.com>
2024-10-19 20:33:47 +0200

    added icons for location and date

--- src/webComponents/Trip/Trip.jsx
+++ src/webComponents/Trip/Trip.jsx
@@ -5,8 +5,14 @@ function Trip(props) {
     return (
         <div className="trip-box">
             <h1 className="name">{props.name}</h1>
-            <h2 className="location">{props.destination}</h2>
-            <h2 className="date">{props.startDate}</h2>
+            <div className="location-wrapper">
+                <i className="fas fa-map-signs"></i>
+                <h2 className="location">{props.destination}</h2>
+            </div>
+            <div className="date-wrapper">
+                <i className="fas fa-calendar-alt"></i>
+                <h2 className="date">{props.startDate}</h2>
+            </div>
         </div>
     )
 }
