9692e3f2f5b214e65f16255b560a5d0420009841
Shirley <smbiayuk@gmail.com>
2024-10-19 19:19:28 +0200

    fix name trip box

--- src/webPages/Trips/Trips.jsx
+++ src/webPages/Trips/Trips.jsx
@@ -1,10 +1,32 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import CreateTripArea from "../../webComponents/CreateTripArea/CreateTripArea";
 import './Trips.css';
 import Trip from "../../webComponents/Trip/Trip";
 
 function Trips() {
     const [trips, setTrips] = useState([]);
+    const [userId] = useState(1);
+
+    const fetchTripsData = async () => {
+        const baseUrl = "http://localhost:8080/trips";
+        const allTripsByUserAPI = `${baseUrl}/getAllTrips?userId=${userId}`;
+        try {
+            const response = await fetch(allTripsByUserAPI); // Fetch trips data from API
+            if (!response.ok) {
+                throw new Error(`HTTP error! status: ${response.status}`);
+            }
+            const tripsData = await response.json(); // Parse response as JSON
+            
+            setTrips(tripsData); // Update the state with trips data
+        } catch (error) {
+            console.error('Error fetching catalog data:', error);
+        }
+    };
+
+    useEffect(() => {
+        // Fetch the trips data on mount
+        fetchTripsData();
+    })
 
     // function addTrip(trip) {
     //     setTrips((prevTrips) => {
@@ -22,7 +44,7 @@ function Trips() {
         setTrips((prevTrips) => [
             ...prevTrips, 
             {
-                tripName: trip.name,
+                name: trip.name,
                 startDate: trip.startDate,
                 endDate: trip.endDate,
                 destination: trip.destination,
@@ -38,7 +60,7 @@ function Trips() {
                 <Trip 
                 key={index}
                 id={index}
-                tripName={trip.name} 
+                name={trip.name} 
                 startDate={trip.startDate}
                 destination={trip.destination}
                 />
